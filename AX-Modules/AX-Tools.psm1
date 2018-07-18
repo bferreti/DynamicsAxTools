8a083afeb74bbd157ee8fbac26403f0cb76657d0
Bruno Ferreti <bferreti@microsoft.com>
2018-07-18 05:54:50 -0700

    Commit

--- AX-Modules/AX-Tools.psm1
+++ AX-Modules/AX-Tools.psm1
@@ -451,9 +451,12 @@ param (
     #Message Parameters
     $SMTPMessage = New-Object System.Net.Mail.MailMessage
     $SMTPMessage.From = $SMTPFrom
-    if(-not [System.DBNull]::Value.Equals($SMTPTo)) {$SMTPTo.Split(';') | % { $SMTPMessage.To.Add($_.Trim()) }} else { break }
-    if(-not [System.DBNull]::Value.Equals($SMTPCC)) {$SMTPCC.Split(';') | % { $SMTPMessage.CC.Add($_.Trim()) }}
-    if(-not [System.DBNull]::Value.Equals($SMTPBCC)) {$SMTPBCC.Split(';') | % { $SMTPMessage.Bcc.Add($_.Trim()) }}
+    #if(-not [System.DBNull]::Value.Equals($SMTPTo)) {$SMTPTo.Split(';') | % { $SMTPMessage.To.Add($_.Trim()) }} else { break }
+    #if(-not [System.DBNull]::Value.Equals($SMTPCC)) {$SMTPCC.Split(';') | % { $SMTPMessage.CC.Add($_.Trim()) }}
+    #if(-not [System.DBNull]::Value.Equals($SMTPBCC)) {$SMTPBCC.Split(';') | % { $SMTPMessage.Bcc.Add($_.Trim()) }}
+    if(-not [string]::IsNullOrEmpty($SMTPTo)) {$SMTPTo.Split(';') | % { $SMTPMessage.To.Add($_.Trim()) }} else { break }
+    if(-not [string]::IsNullOrEmpty($SMTPCC)) {$SMTPCC.Split(';') | % { $SMTPMessage.CC.Add($_.Trim()) }}
+    if(-not [string]::IsNullOrEmpty($SMTPBCC)) {$SMTPBCC.Split(';') | % { $SMTPMessage.Bcc.Add($_.Trim()) }}
     $SMTPMessage.Subject = $Subject
     $SMTPMessage.IsBodyHtml = $true
     $SMTPMessage.Body = $Body
@@ -1342,5 +1345,91 @@ param (
     }
     else {
         Write-Log "ERROR: Perfmon Check environment $AXEnvironment not found."
+    }
+}
+
+function New-Popup {
+<#
+.Synopsis
+Display a Popup Message
+.Description
+This command uses the Wscript.Shell PopUp method to display a graphical message
+box. You can customize its appearance of icons and buttons. By default the user
+must click a button to dismiss but you can set a timeout value in seconds to 
+automatically dismiss the popup. 
+
+The command will write the return value of the clicked button to the pipeline:
+  OK     = 1
+  Cancel = 2
+  Abort  = 3
+  Retry  = 4
+  Ignore = 5
+  Yes    = 6
+  No     = 7
+
+If no button is clicked, the return value is -1.
+.Example
+PS C:\> new-popup -message "The update script has completed" -title "Finished" -time 5
+
+This will display a popup message using the default OK button and default 
+Information icon. The popup will automatically dismiss after 5 seconds.
+.Outputs
+Null   = -1
+OK     = 1
+Cancel = 2
+Abort  = 3
+Retry  = 4
+Ignore = 5
+Yes    = 6
+No     = 7
+#>
+
+Param (
+[Parameter(Position=0,Mandatory=$True,HelpMessage="Enter a message for the popup")]
+[ValidateNotNullorEmpty()]
+[string]$Message,
+[Parameter(Position=1,Mandatory=$True,HelpMessage="Enter a title for the popup")]
+[ValidateNotNullorEmpty()]
+[string]$Title,
+[Parameter(Position=2,HelpMessage="How many seconds to display? Use 0 require a button click.")]
+[ValidateScript({$_ -ge 0})]
+[int]$Time=0,
+[Parameter(Position=3,HelpMessage="Enter a button group")]
+[ValidateNotNullorEmpty()]
+[ValidateSet("OK","OKCancel","AbortRetryIgnore","YesNo","YesNoCancel","RetryCancel")]
+[string]$Buttons="OK",
+[Parameter(Position=4,HelpMessage="Enter an icon set")]
+[ValidateNotNullorEmpty()]
+[ValidateSet("Stop","Question","Exclamation","Information" )]
+[string]$Icon="Information"
+)
+    Switch ($Buttons) {
+        "OK"               {$ButtonValue = 0}
+        "OKCancel"         {$ButtonValue = 1}
+        "AbortRetryIgnore" {$ButtonValue = 2}
+        "YesNo"            {$ButtonValue = 4}
+        "YesNoCancel"      {$ButtonValue = 3}
+        "RetryCancel"      {$ButtonValue = 5}
+    }
+
+    #set an integer value for Icon type
+    Switch ($Icon) {
+        "Stop"        {$iconValue = 16}
+        "Question"    {$iconValue = 32}
+        "Exclamation" {$iconValue = 48}
+        "Information" {$iconValue = 64}
+    }
+
+    #create the COM Object
+    Try {
+        $wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
+        #Button and icon type values are added together to create an integer value
+        $Answer = $wshell.Popup($Message,$Time,$Title,$ButtonValue+$iconValue)
+        return $Answer
+    }
+    Catch {
+        #You should never really run into an exception in normal usage
+        Write-Warning "Failed to create Wscript.Shell COM object"
+        Write-Warning $_.exception.message
     }
 }
